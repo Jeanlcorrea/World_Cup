69f15d4d8408466d096e9009a2ccb11ff517d517
Jean <jeanlucorrrea@gmail.com>
2022-10-04 18:47:25 -0300

    world cup

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '18px'],
+      base: ['14px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '2xl': ['32px', '40px'],
+      '3xl': ['48px', '56px'],
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
